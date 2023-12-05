8cfd65768e0d00abf098ddfdc4d4f8aadade5631
glproj <glprojetinho@gmail.com>
2023-12-05 11:58:39 -0300

    feat(tamper): make first facilitador version

--- tampermonkey/facilitador.js
+++ tampermonkey/facilitador.js
@@ -1,6 +1,21 @@
 "use strict";
-let imprimirBotao = $("[title=\"Imprimir\"]");
-let imprimirCelula = imprimirBotao.parent();
-let numeroDePedidoCelula = imprimirCelula.next();
-let nomeEmpresaCelula = numeroDePedidoCelula.next();
-let tipoPedidoCelula = nomeEmpresaCelula.next();
+let printButton = $("[title=Imprimir]");
+const updateURL = "http://localhost:8080/update"
+printButton.on("click", function () {
+    let clickedButton = $(this)
+    let printCell = clickedButton.parent().parent();
+    let orderNumber = printCell.next().text();
+    let businessName = printCell.next().next().text();
+    let orderType = printCell.next().next().next().text();
+    let queryParams = { "businessName": businessName, "orderNumber": orderNumber, "orderType": orderType }
+    let queryURL = new URL(updateURL)
+    queryURL.search = new URLSearchParams(queryParams).toString()
+    fetch(queryURL).then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    }).catch(error => {
+        console.error('Error:', error);
+    });
+})
