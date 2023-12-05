6c3ac26e0ee05d2179c111693f9f401f80d5d22a
glproj <glprojetinho@gmail>
2023-12-05 06:19:53 -0300

    feat(WA): add updateRow

--- webapp/main.go
+++ webapp/main.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/xuri/excelize/v2"
 )
 
+func updateRow(orderNumber int, businessName string, orderType string, file *excelize.File) error {
+	orderNumberStr := strconv.Itoa(orderNumber)
+	sheet := file.GetSheetList()[0]
+	rows, err := file.GetRows(sheet)
+	if err != nil {
+		return err
+	}
+	firstEmptyRowIndex := strconv.Itoa(len(rows) + 1)
+	err = file.SetCellValue(sheet, "A"+firstEmptyRowIndex, orderNumberStr)
+	if err != nil {
+		return err
+	}
+	err = file.SetCellValue(sheet, "B"+firstEmptyRowIndex, businessName)
+	if err != nil {
+		return err
+	}
+	err = file.SetCellValue(sheet, "C"+firstEmptyRowIndex, orderType)
+	if err != nil {
+		return err
+	}
+
+	currentDateTime := time.Now().Format("15:04:05")
+	err = file.SetCellValue(sheet, "E"+firstEmptyRowIndex, currentDateTime)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 func main() {
 	f, err := excelize.OpenFile("test.xlsx")
 	if err != nil {
