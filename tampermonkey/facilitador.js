01bc9f7b61edaef55081df2369a25594275e7b54
glproj <glprojetinho@gmail>
2023-12-06 19:08:36 -0300

    feat(tamper): implement facilitador script

--- tampermonkey/facilitador.js
+++ tampermonkey/facilitador.js
@@ -1,21 +1,61 @@
 "use strict";
-let printButton = $("[title=Imprimir]");
-const updateURL = "http://localhost:8080/update"
-printButton.on("click", function () {
-    let clickedButton = $(this)
-    let printCell = clickedButton.parent().parent();
-    let orderNumber = printCell.next().text();
-    let businessName = printCell.next().next().text();
-    let orderType = printCell.next().next().next().text();
-    let queryParams = { "businessName": businessName, "orderNumber": orderNumber, "orderType": orderType }
-    let queryURL = new URL(updateURL)
-    queryURL.search = new URLSearchParams(queryParams).toString()
-    fetch(queryURL).then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-    }).catch(error => {
-        console.error('Error:', error);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-})
+};
+let printButton = $("[title=Imprimir]");
+const updateURL = "http://localhost:8080/update";
+printButton.on("click", function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        let clickedButton = $(this);
+        let printCell = clickedButton.parent().parent();
+        let orderNumber = printCell.next().text().trim();
+        let businessName = printCell.next().next().text().trim();
+        let orderType = printCell.next().next().next().next().text().trim();
+        let queryParams = {
+            businessName: businessName,
+            orderNumber: orderNumber,
+            orderType: orderType,
+            checkForDuplicate: "true",
+        };
+        let queryURL = new URL(updateURL);
+        queryURL.search = new URLSearchParams(queryParams).toString();
+        let res = yield fetch(queryURL)
+            .then((response) => __awaiter(this, void 0, void 0, function* () {
+            if (!response.ok) {
+                let message = yield response.json();
+                throw new Error(message);
+            }
+            console.log(response);
+        }))
+            .catch((error) => __awaiter(this, void 0, void 0, function* () {
+            if (error.message.includes("Found another row")) {
+                let goOn = confirm("Há um pedido com o mesmo número na tabela. Desejas prosseguir mesmo assim?");
+                if (goOn) {
+                    queryParams.checkForDuplicate = "";
+                    queryURL = new URL(updateURL);
+                    queryURL.search = new URLSearchParams(queryParams).toString();
+                    let res = yield fetch(queryURL)
+                        .then((response) => __awaiter(this, void 0, void 0, function* () {
+                        if (!response.ok) {
+                            let message = yield response.json();
+                            alert(message);
+                        }
+                        console.log(response);
+                    }))
+                        .catch((error) => {
+                        alert(error.message);
+                    });
+                }
+            }
+            else {
+                alert(error);
+            }
+        }));
+    });
+});
--- tampermonkey/facilitador.ts
+++ tampermonkey/facilitador.ts
@@ -1,7 +1,62 @@
-let imprimirBotao = $("[title=\"Imprimir\"")
-let imprimirCelula = imprimirBotao.parent()
-let numeroDePedidoCelula = imprimirCelula.next()
-let nomeEmpresaCelula = numeroDePedidoCelula.next()
-let tipoPedidoCelula = nomeEmpresaCelula.next()
+interface UpdateRowParams {
+  businessName: string;
+  orderNumber: string;
+  orderType: string;
+  checkForDuplicate?: string;
+}
 
-console.log(numeroDePedidoCelula.text())
+let printButton = $("[title=Imprimir]");
+const updateURL = "http://localhost:8080/update";
+printButton.on("click", async function () {
+  let clickedButton = $(this);
+  let printCell = clickedButton.parent().parent();
+  let orderNumber = printCell.next().text().trim();
+  let businessName = printCell.next().next().text().trim();
+  let orderType = printCell.next().next().next().next().text().trim();
+
+  let queryParams: UpdateRowParams = {
+    businessName: businessName,
+    orderNumber: orderNumber,
+    orderType: orderType,
+    checkForDuplicate: "true",
+  };
+  let queryURL = new URL(updateURL);
+  queryURL.search = new URLSearchParams(
+    queryParams as Record<any, any>
+  ).toString();
+  let res = await fetch(queryURL)
+    .then(async (response) => {
+      if (!response.ok) {
+        let message = await response.json();
+        throw new Error(message);
+      }
+      console.log(response);
+    })
+    .catch(async (error: Error) => {
+      if (error.message.includes("Found another row")) {
+        let goOn = confirm(
+          "Há um pedido com o mesmo número na tabela. Desejas prosseguir mesmo assim?"
+        );
+        if (goOn) {
+          queryParams.checkForDuplicate = "";
+          queryURL = new URL(updateURL);
+          queryURL.search = new URLSearchParams(
+            queryParams as Record<any, any>
+          ).toString();
+          let res = await fetch(queryURL)
+            .then(async (response) => {
+              if (!response.ok) {
+                let message = await response.json();
+                alert(message);
+              }
+              console.log(response);
+            })
+            .catch((error) => {
+              alert(error.message);
+            });
+        }
+      } else {
+        alert(error);
+      }
+    });
+});
